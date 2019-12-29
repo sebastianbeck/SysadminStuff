f2b78f681af6eb4433db44dbc2d2ec429c3548aa
sebastianbeck <19960761+sebastianbeck@users.noreply.github.com>
2019-12-29 14:24:28 +0100

    Update notes.py

--- notes.py
+++ notes.py
@@ -96,3 +96,132 @@ print(t, formatted_string)
 d = datetime.date(year=1997, month=1, day=6)
 formatted_string = d.strftime("%A %d %B %y")
 print(d, formatted_string)
+#################################################################################################################
+#Break to improve efficency
+
+
+# Find the index (with break)
+def find_index():
+    lst = [976, 618, 812, 898, 804, 689, 611, 630, 467, 411, 648, 931, 618]
+    index = 0
+    iteration_count = 0
+    for num in lst:
+        print(num)
+        if (num == 611):
+            found_at = index
+            break # adding a break to improve efficiency
+        index = index + 1
+        iteration_count = iteration_count + 1
+    print("Using a break, 611 was found at index:", found_at, "using", iteration_count, "iterations")
+
+find_index()
+
+#break infinite loop
+def infinite_loop():
+    while True:
+        x = int(input("Enter number between 1-5: "))
+        if(x>0):
+            break
+#infinite_loop()
+
+def char_art(steps):
+    for row in range(steps):
+        for col in range(steps):
+            if(col <= row):
+                print("[]", end ="")
+        print()
+#char_art(6)
+
+def sum_of_rows():
+    table = [[5, 2, 6], [4, 6, 0], [9, 1, 8], [7, 3, 8]]
+    for row in table:
+        sum = 0
+        for col in row:
+            sum = sum + col
+        print(sum)     
+#sum_of_rows()
+        
+#check if somethings in list
+def in_list(x):
+    lst_container = [4,5,6,7,8]
+    if(x in lst_container):
+        print("it is in it")
+    else:
+        print("its not")
+    lst_container = [4, [7, 3], 'string element']
+
+    # 4 is an element in lst_container
+    x = 4
+    print(x, "contained in lst_container:", x in lst_container)
+
+    # 7 is an element of a list inside lst_container, but it is NOT an element of the lst_container
+    x = 7
+    print(x, "contained in lst_container:", x in lst_container)
+
+    # [7, 3] is an element of lst_container
+    x = [7, 3]
+    print(x, "contained in lst_container:", x in lst_container)
+    in_list(10)
+
+#equal and identical
+def eq():
+    # x, y: equal, identical 
+    x = 5
+    y = 5
+    print("x equal y ? ", x == y)
+    print("x is identical to y ?", x is y)
+    x = 5.6
+    y = 5.6
+    print("x equal y ? ", x == y)
+    print("x is identical to y ?", x is y)
+
+    # Different lists containing the same data
+    x = [4, 9, 8]
+    y = [4, 9, 8]
+
+    # x and y are equal, because they contain the same data
+    # x and y are NOT identical, because they are saved in different memory locations
+    print()
+    print("x equal y ? ", x == y)
+    print("x is identical to y ?", x is y)
+
+    # Because they are not identical, changing x does not affect y
+    x[1] = 5
+
+    print()
+    print("After changing x[1]")
+    print("x =", x)
+    print("y =", y)
+    print("x equal y ? ", x == y)
+    print("x is identical to y ?", x is y)
+
+    # s1, s2: equal, not identical
+    s1 = 'whole milk'
+    s2 = 'whole milk'
+    print("s1 equal s2 ? ", s1 == s2)
+    print("s1 is identical to s2 ?", s1 is s2)
+    print("s1 is not identical to s2 ?", s1 is not s2)
+#eq()
+
+#string formattierung
+
+def strformating():
+    l=40
+    km=450
+    print('The fuel efficiency of a car consuming {} liter of gas for every {} km = {} km/l'.format(l, km, km/l))
+    print('The fuel efficiency of a car consuming {0:d} liter of gas for every {1:4.2f} km = {2:4.1f} km/l'.format(l, km, km/l))
+    print('The fuel efficiency of a car consuming {a:d} liter of gas for every {b:4.2f} km = {c:4.2f} km/l'.format(a = l, b = km, c = km/l))
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    print('The first name starts with: {:s}'.format(first_name[0]))
+    print('The last name ends with the 2 chars: {:s}'.format(last_name[-2:]))
+    from math import pi
+
+    print('Right adjusted : {:<20.2f}'.format(pi))
+    print('Center adjusted: {:^20.2f}'.format(pi))
+    print('Left adjusted  : {:>20.2f}'.format(pi))
+    print('Right adjusted : {:_<20.2f}'.format(pi))
+    print('Center adjusted: {:_^20.2f}'.format(pi))
+    print('Left adjusted  : {:_>20.2f}'.format(pi))
+
+strformating()
